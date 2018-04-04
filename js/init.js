37f6ecb0a9ae0fc9be5713d93a2edb88f04822dc
cweet-dream <svetlana.apolokhova@gmail.com>
2018-04-04 12:46:13 +0300

    adaptive view added

--- js/init.js
+++ js/init.js
@@ -9,4 +9,9 @@ $( function() {
       prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>', 
       nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>', 
     });
+    // Init slicknav
+    $('.header-nav').slicknav({
+      appendTo: '.header .container-fluid',
+      label:''
+    })
   });
